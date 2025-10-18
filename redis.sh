0dcac9a66a127d95abf5ab02bd2c83a70048ed8d
Sudeep <sudeepreddy340@gmail.com>
2025-10-18 15:59:46 +0530

    commit

--- redis.sh
+++ redis.sh
@@ -10,10 +10,11 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 MONGODB_HOST=172.31.22.68
 SCRIPT_PATH=/home/ec2-user/shell-roboshop/
+START_TIME=$(date +%s)
 
 
 mkdir -p $LOGS_FOLDER
-echo "script started executed at: $(date)"
+echo "script started execution at: $(date)"
 
 USERID=$(id -u)
 
@@ -49,3 +50,7 @@ systemctl enable redis &>>LOG_FILE
 VALIDATE $? "Enabling redis"
 systemctl start redis &>>LOG_FILE
 VALIDATE $? "Starting redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $Start_TIME))
+echo -e "Script executed in: $Y $TOTAL_TIME Secong $N"
