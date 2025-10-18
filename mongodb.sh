02de730f686d2f4286fa5c00a6bc02ef8e03a844
Sudeep <sudeepreddy340@gmail.com>
2025-10-18 11:13:48 +0530

    commit

---
+++ mongodb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root access"
+    exit 1 # failure is other than 0
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 Failure"
+        exit 1
+    else    
+        echo " $2 Success"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongodb
+VALIDATE $? "Enabled MongoDB"
+
+systemctl start mongodb
+VALIDATE $? "Started MongoDB"
